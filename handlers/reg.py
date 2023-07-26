2b971eddfaefd60c15ebb67ddbfd6cf3a00a3b0a
Vitaliy <harchenko.grape@gmail.com>
2023-07-26 08:52:01 +0500

    remove unuser handlers

--- handlers/reg.py
+++ handlers/reg.py
@@ -92,34 +92,3 @@ async def phone_chosen(message: Message, state: FSMContext):
 
     await message.answer(f"Твой набор данных:{user_data}")
     await state.clear()
-
-
-
-
-
-
-
-
-@router.message(Command("contact"))
-async def contact_handler(msg: Message):
-    kb = [
-        [types.KeyboardButton(text="Дать доступ", request_contact=True)]
-    ]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True, input_field_placeholder="Предоставьте доступ к номеру телефона для идентефикации")
-    await msg.answer("Привет! Для регистрации поделись своим телефоном. К нему будет привязан профиль с твоей статистикой", reply_markup=keyboard)
-
-
-@router.message(F.content_type.in_({'contact'}))
-async def send_contact_handler(msg: Message):
-    await msg.answer(f"Получен телефон: {msg.contact.phone_number}")
-
-
-@router.message(Command("check"))
-async def check_reg_handler(msg: Message):
-    try:
-        url = f'http://127.0.0.1:8000/users/check/telegram/{msg.from_user.id}/'
-        response = urlopen(url)
-        user =  json.load(response)
-        await msg.answer(f"{user['first_name']} {user['last_name']}, вы зарегистрированы: {user}")
-    except:
-        await msg.answer(f"Вы не зарегистрированы")
